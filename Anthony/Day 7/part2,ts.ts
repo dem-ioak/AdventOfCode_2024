7be026368679dc99dee514a18080776f4231f41f
Anthony Lawn <anthony.lawn@topazlabs.com>
2024-12-07 07:34:29 -0600

    Added early exit case 3.5s -> 2.1s

--- Anthony/Day 7/part2,ts.ts
+++ Anthony/Day 7/part2,ts.ts
@@ -15,7 +15,7 @@ for (let equation of input.trim().split("\r\n")) {
 	const operands = operandsString.trim().split(" ").map(v => parseInt(v));
 	console.log("Trying to calculate", result, "with", operands)
 
-	for (let value of generateResults(operands)) {
+	for (let value of generateResults(result, operands)) {
 		if (value === result) {
 			sum += result;
 			break;
@@ -23,13 +23,17 @@ for (let equation of input.trim().split("\r\n")) {
 	}
 }
 
-function* generateResults(operands) {
+function* generateResults(target, operands) {
+	if (operands[0] > target) {
+		// Early exit
+		return;
+	}
 	if (operands.length === 1) {
 		yield operands[0];
 		return;
 	}
 	for (let operator of operators) {
-		yield* generateResults([operator(operands[0], operands[1]), ...operands.slice(2)]);
+		yield* generateResults(target, [operator(operands[0], operands[1]), ...operands.slice(2)]);
 	}
 }
 
