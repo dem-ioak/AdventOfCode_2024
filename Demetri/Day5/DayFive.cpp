ea3d342e642babe97853c1631b7255fe8ce32f26
Demetri Ioakimidis <dem.ioak@gmail.com>
2024-12-14 17:47:21 -0500

    Minor 5.1 changes

--- Demetri/Day5/DayFive.cpp
+++ Demetri/Day5/DayFive.cpp
@@ -5,62 +5,114 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <algorithm>
+
+std::vector<int> split(std::string& line, char delimiter) {
+    std::istringstream iss(line);
+    std::vector<int> tokens;
+    std::string curr_token;
+    while (std::getline(iss, curr_token, delimiter)) {
+        tokens.push_back(std::stoi(curr_token));
+    }
+    return tokens;
+}
+
+void print_map(std::unordered_map<int, std::unordered_set<int>>& hmap) {
+    for (const auto& [key, lst] : hmap) {
+        std::cout << key << " -> [";
+        for (const auto& value : lst) {
+            std::cout << value << ',';
+        }
+        std::cout << ']' << std::endl;
+    }
+}
 
 int part_one(const std::string& file_name) {
-    std::fstream file(file_name);
+    std::ifstream file(file_name);
     std::string curr_line;
-    std::string token;
-    char delimiter;
-    int left, right, curr, total = 0;
-    std::unordered_map<int, std::vector<int>> ruleset;
-    std::unordered_set<int> seen;
-    std::vector<int> entries;
-    bool correct;
+    bool is_rule;
+    std::unordered_map<int, std::unordered_set<int>> ruleset;
+    std::vector<int> tokens;
+    int left, right;
+    int total = 0;
     while (std::getline(file, curr_line)) {
+        if (curr_line.empty()) {
+            continue;
+        }
+        
         std::istringstream iss(curr_line);
-
-        // Read in rules
-        if (curr_line.find('|') != std::string::npos) {
-            std::getline(iss, token, '|');
-            left = std::stoi(token);
-            std::getline(iss, token, '|');
-            right = std::stoi(token);
-            ruleset[left].push_back(right);
+        is_rule = curr_line.find('|') != std::string::npos;
+        // Create map linking N to NUMBERS THAT MUST COME BEFORE N
+        if (is_rule) {
+            tokens = split(curr_line, '|');
+            left = tokens[0];
+            right = tokens[1];
+            ruleset[left].insert(right);
         }
 
-        // Evaluate lines
+        // Validate input
         else {
-            seen.clear();
-            entries.clear();
-            correct = true;
-            while (std::getline(iss, token, ',')) {
-                curr = std::stoi(token);
-                seen.insert(curr);
-                entries.push_back(curr);
-                // If the current value needs to appear before some values
-                if (ruleset.find(curr) != ruleset.end()) {
-                    // Check if any of those values have already been seen
-                    // If so, wrong order
-                    for (auto& condition : ruleset[curr]) {
-                        if (seen.find(condition) != seen.end()) {
+            std::unordered_set<int> seen;
+            bool correct = true;
+            tokens = split(curr_line, ',');
+            for (auto& ele : tokens) {
+                seen.insert(ele);
+                if (ruleset.find(ele) != ruleset.end()) {
+                    for (auto& dependency : ruleset[ele]) {
+                        if (seen.find(dependency) != seen.end()) {
                             correct = false;
                             break;
                         }
                     }
+                    if (!correct) {
+                        break;
+                    }
                 }
-            }
-            total += (correct ? entries[entries.size() / 2] : 0);
+            }   
+            total += correct ? tokens[tokens.size() / 2] : 0;
         }
     }
     return total;
 }
 
-int part_two() {
+int part_two(const std::string& file_name) {
+    std::ifstream file(file_name);
+    std::string curr_line;
+    bool is_rule;
+    std::unordered_map<int, std::vector<int>> ruleset;
+    std::unordered_set<int> uniques;
+    std::vector<int> tokens;
+    int left, right;
+    int total = 0;
+    while (std::getline(file, curr_line)) {
+        if (curr_line.empty()) {
+            continue;
+        }
+        
+        std::istringstream iss(curr_line);
+        is_rule = curr_line.find('|') != std::string::npos;
+        // Create map linking N to NUMBERS THAT MUST COME BEFORE N
+        if (is_rule) {
+            tokens = split(curr_line, '|');
+            left = tokens[0];
+            right = tokens[1];
+            ruleset[left].push_back(right);
+            uniques.insert(left);
+            uniques.insert(right);
+        }
+
+        // Validate input
+        else {
+            tokens = split(curr_line, ',');
+            // I'll figure this out eventually
+        }
+    }
     return 1;
 }
 
 int main() {
     const std::string file_name = "day_five_input.txt";
     std::cout << part_one(file_name) << std::endl;
+    // std::cout << part_two(file_name) << std::endl;
 
 }
