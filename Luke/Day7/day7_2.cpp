ba5d04de453f88027238eaa0a4f78e3104abaed6
Luke <LukeRocheDev@gmail.com>
2024-12-16 08:10:22 -0600

    Luke Day7 (#13)
    * Day7 part 1
    
    * Cleanup
    
    * Day7 part 2

---
+++ Luke/Day7/day7_2.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+typedef unsigned long long ull;
+
+bool evaluate(std::vector<ull>& nums, ull target, ull total = 0, ull index = 0) {
+    if(index == nums.size()) return target == total;
+    if(index >= nums.size()) return false;
+
+    if(index == 0) {
+        return evaluate(nums, target, nums[index], index+1);
+    }else{
+        auto addPath = evaluate(nums, target, total + nums[index], index+1);
+        auto mulPath = evaluate(nums, target, total * nums[index], index+1);
+        auto conPath = evaluate(nums, target, std::stoll(std::to_string(total) + std::to_string(nums[index])), index+1);
+        return addPath || mulPath || conPath;
+    }
+}
+
+int main() {
+    std::ifstream file("puzzle_input.txt");
+
+    std::string line;
+    ull sumTestValues = 0;
+    while(std::getline(file, line)) {
+        ull testValue = std::stoll(line.substr(0, line.find(':')));
+
+        std::istringstream stream(line.substr(line.find(':') + 1));
+        std::vector<ull> numbers;
+        ull number;
+        
+        while(stream >> number) numbers.push_back(number);
+        if (evaluate(numbers, testValue)) sumTestValues += testValue;
+    }
+
+    printf("Sums of testvalues: %llu\n", sumTestValues);
+}
